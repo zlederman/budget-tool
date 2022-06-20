fc6f7917d3072f63e3b589f339a19d949dede265
Zachary Lederman <zlederma@purdue.edu>
2022-06-20 16:50:31 -0500

    hack didnt work

--- server.js
+++ server.js
@@ -5,26 +5,12 @@ const app = express()
 const smsRoute = require('./routes/sms.route')
 require('dotenv').config()
 
-const waitForEnv = () =>{
-    if(typeof process.env.USERNAME !== undefined){
-        mongoose.connect(uri,{ 
-            useNewUrlParser: true,
-            useUnifiedTopology: true })
-            .then(()=>console.log('connected'))
-            .catch((err)=> console.log(err))
-    
-    }
-    else{
-        setTimeout(waitForEnv,250)
-    }
-}
-waitForEnv()
-// const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASS}@cluster0.kgaoe.mongodb.net/db?retryWrites=true&w=majority`;
-// mongoose.connect(uri,{ 
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true })
-//         .then(()=>console.log('connected'))
-//         .catch((err)=> console.log(err))
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASS}@cluster0.kgaoe.mongodb.net/db?retryWrites=true&w=majority`;
+mongoose.connect(uri,{ 
+        useNewUrlParser: true,
+        useUnifiedTopology: true })
+        .then(()=>console.log('connected'))
+        .catch((err)=> console.log(err))
 
 
 app.use('/',smsRoute)
