1bc70cc3bc897e3a1a443160541d2d3c7da2f4d3
Zach <zlederman00@gmail.com>
2022-07-08 16:42:15 -0500

    it may not be pretty but it works

--- frontend/src/components/CreateConfigForm.jsx
+++ frontend/src/components/CreateConfigForm.jsx
@@ -11,39 +11,34 @@ import {
     Text
 } from '@chakra-ui/react'
 import {DeleteIcon} from '@chakra-ui/icons'
-
+import uuid from 'react-uuid'
 const ConfigContext = React.createContext({})
 const ConfigProvider = ConfigContext.Provider;
 
 const LiItem = (props) =>{
-    // const {onDelete} = useContext(ConfigContext)
+    const context = React.useContext(ConfigContext)
+
     return(
         <ListItem marginBottom='2'  flexDirection='row' display='flex'>
-            <Input bg='gray.200'  borderWidth='0' onChange={(e)=> {}}/>
-            <Button marginLeft='3px' colorScheme='red' onClick={()=> props.deleter(props.id)}><DeleteIcon/></Button>
+            <Input bg='gray.200'  borderWidth='0' onChange={(e)=> {context.onChange({text:e.target.value, idx:props.idx})}}/>
+            <Button marginLeft='3px' colorScheme='red' onClick={()=> context.onDelete(props.id)}><DeleteIcon/></Button>
         </ListItem>
     )
 }
 const DynamicTextList = (props) => {
-    
-    const expandListItem = (props) => {
-        return props.listItems.map((x,idx)=> <LiItem key={idx} deleter={props.deleter} id={x}/>)
+    const context = React.useContext(ConfigContext)
+    const expandListItem = () => {
+        return 
     }
     return(
         <List>
-            {expandListItem(props)}
+            {context.data.map((x,idx)=> <LiItem key={x.id}  id={x} idx={idx} />)}
         </List>
     )
 }
-const FormBlock = (props) => {
-    const [items,setItems] = React.useState([])
 
-    function appendItems(item) {
-        setItems(oldArr => [...oldArr,item])
-    }
-    function deleteItem(item) {
-        setItems(items.filter((key)=>key !== item))
-    }
+const FormBlock = (props) => {
+
     return(
         <Box marginBottom='9' borderRadius='5px' boxShadow='md'>
             <Heading size='md'color='white'>
@@ -52,8 +47,8 @@ const FormBlock = (props) => {
             <Text color='gray.400'>
                 <i>{props.subHeading}</i>
             </Text>
-            <DynamicTextList listItems={items}  deleter={deleteItem}/>
-            <Button backgroundColor='gray.600' color='gray.200' minWidth='100%' onClick={()=>{appendItems(items.length+1)}}>+</Button>
+            <DynamicTextList />
+            <Button backgroundColor='gray.600' color='gray.200' minWidth='100%' onClick={()=>{props.onAppend({id: uuid() ,text:""})}}>+</Button>
         </Box>
 
     )
@@ -61,17 +56,48 @@ const FormBlock = (props) => {
 const ConfigForm = (props) => {
     const [typeItems,setTypes] = React.useState([])
     const [methodItems,setMethods] = React.useState([])
-    const [typeData, setTypeData] = React.useState([])
-
-    function addTypeInp(next) {
-        setTypes(oldArr => [...oldArr,next])
+   
+    function appendType(item) {
+        setTypes(oldArr => [...oldArr,item])
     }
-    function addMethodInp(next) {
-        setMethods(oldArr => [...oldArr,next])
+    function deleteType(id) {
+        let newTypes= typeItems.filter((item)=>item.id !== id.id)
+        setTypes(newTypes)
+    }
+    function changeType(obj){
+        let types = [...typeItems]
+        types[obj.idx].text = obj.text
+        setTypes(types)
+    }
+    function appendMethod(item) {
+        setMethods(oldArr => [...oldArr,item])
+    }
+    function deleteMethod(id) {
+        let newMethods= methodItems.filter((item)=>item.id !== id.id)
+        setMethods(newMethods)
+    }
+    function changeMethod(obj){
+        let methods = [...methodItems]
+        methods[obj.idx].text = obj.text
+        setTypes(methods)
     }
 
+    const contextValue = {
+        types : {
+            onDelete: deleteType,
+            onChange: changeType,
+            onAppend: appendType,
+            data: typeItems
+        },
+        methods : {
+            onDelete: deleteMethod,
+            onAppend: appendMethod,
+            onChange: changeMethod,
+            data:methodItems
+        }
+    }
     return (
-        <ConfigProvider >
+    
             <Flex
                 flexDirection="column"
                 width="100wh"
@@ -87,21 +113,35 @@ const ConfigForm = (props) => {
                     <Heading size='2xl'marginBottom='4'color='white'>
                         Configure Your Budget Buddy
                     </Heading>
-                    <FormBlock 
-                        heading='Add your purchase types'
-                        subHeading='e.g. essentials, fun, food'
-                    />
+                    <ConfigProvider value={contextValue.types}>
+                        <FormBlock
+                            onAppend={appendType}
+                            // onDelete={deleteType}
+                            // onChange={changeType}
+                            items={typeItems}
+                            heading='Add your purchase types'
+                            subHeading='e.g. essentials, fun, food'
+                        />
+                    </ConfigProvider>
+
                     <Box padding='4'></Box>
-                    <FormBlock 
-                        heading='Add your payment methods'
-                        subHeading='e.g. credit, debit, cash'
-                    />
+
+                    <ConfigProvider value={contextValue.methods}>
+                        <FormBlock
+                            onAppend={appendMethod}
+                            items={methodItems}
+                            heading='Add your payment methods'
+                            subHeading='e.g. credit, debit, cash'
+                        />
+                        
+                    </ConfigProvider>
                     <Box padding='3'></Box>
                     <Button colorScheme='teal'>Submit</Button>
                     
                 </Stack>
             </Flex>
-        </ConfigProvider>
+ 
+      
         
     )
 }
