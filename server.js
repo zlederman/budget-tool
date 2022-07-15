19ece6f2e300677f6bdec539c8436820ef643ebd
Zach <zlederman00@gmail.com>
2022-07-15 14:07:36 -0500

    added log for visibilty

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,'frontend','build')))
 
 app.get('/',(req,res)=>{
+    console.log(req)
     res.sendFile(path.join(__dirname,'frontend','build','index.html'))
 })
 app.use('/api/sms',smsRoute)
